cdeed6a31e220404c404f7f2e0b0b268b22a6a0d
androidYin <teacheryin.android@gmail.com>
2016-03-17 11:44:19 +0800

    checbox + -  訊息重設

--- app/src/main/java/com/example/android/lab02_interactive/MainActivity.java
+++ app/src/main/java/com/example/android/lab02_interactive/MainActivity.java
@@ -3,62 +3,103 @@ package com.example.android.lab02_interactive;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    // m: Member ( Data Member 資料成員)
-    private int mQuantity = 0; // 初始值
-    private int mPrice = 5;    // 初始值 $5
-    private final String mNT$ = "NT$";
-    private StringBuilder mTotalPriceMessage = new StringBuilder(mNT$); // 建立可重複使用的字串
+
+    private int mQuantity = 0;
+    private int mPrice = 50;
+    private String mName = "鳴人";
+    private StringBuilder mPriceMessage = new StringBuilder("NT$" + mPrice);
+    private StringBuilder mQuantityMessage = new StringBuilder(mQuantity);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        displayPriceMessage(); // 顯示初始的訊息
     }
 
-    // Order 按鈕按下時執行 (好萊塢會找我們)
-    public void submitOrder(View view) {
-
-        displayTotalPrice();
+    private void displayQuantityMessage() {
+        TextView quantityTextView =
+                (TextView) findViewById(R.id.quantity_text_view);
+        // 將數量字串刪除，替換成新的數量值
+        int start = 0;
+        int end = mQuantityMessage.length();
+        mQuantityMessage.delete(start, end).append(mQuantity);
+        quantityTextView.setText(mQuantityMessage);
     }
 
-    private void displayTotalPrice() {
-        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
-        int total = mPrice * mQuantity;
-        int startIndex = mNT$.length();
-        int endIndex = mTotalPriceMessage.length();
-        mTotalPriceMessage.delete(startIndex,endIndex).append(total)
-                .append(mQuantity == 0 ? "\nFree" : "\nThank you!");
-        priceTextView.setText(mTotalPriceMessage);
-    }
-
-    private void displayQuantity() {
-        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(mQuantity));
-    }
-
+    // 按下 + - button
     public void increment(View view) {
-        // 從 TextView 取得目前數量，數量加 1 並顯示
         ++mQuantity;
-        displayQuantity();
-        resetTotolPrice();
+        displayQuantityMessage();
+        resetPriceMessageString(); // 數量改變，重設價格訊息字串
+        displayPriceMessage(); // 重新顯示價格訊息
     }
 
     public void decrement(View view) {
-        // 從 TextView 取得目前數量，數量減 1 並顯示
-        if(mQuantity > 0) {
-            --mQuantity;
-            displayQuantity();
-            resetTotolPrice();
+        if (mQuantity == 0) {
+            return;
+        }
+        --mQuantity;
+        displayQuantityMessage();
+        resetPriceMessageString(); // 數量改變，重設價格訊息字串
+        displayPriceMessage(); // 重新顯示價格訊息
+    }
+
+    private void displayPriceMessage() {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(mPriceMessage);
+    }
+
+    // 按下 ORDER Button
+    public void submitOrder(View view) {
+        clearPriceMessageString(); // 刪除 mPriceMessage
+        concatPriceMessageString(); // 字串組合 人名 + 臭豆腐 + 是否加泡菜 + Free 或 NT$xxx Thank you!
+        displayPriceMessage(); // 重新顯示 mPriceMessage
+    }
+
+    private void clearPriceMessageString() {
+        int start = 0;
+        int end = mPriceMessage.length();
+        mPriceMessage.delete(start, end);
+    }
+
+    private void concatPriceMessageString() {
+        CheckBox checkBox = (CheckBox) findViewById(R.id.toppings_checkbox);
+        mPriceMessage.append("Name: ")
+                .append(mName)
+                .append("\n")
+                .append("臭豆腐")
+                .append("\n")
+                .append("加泡菜 ? ")
+                .append(checkBox.isChecked())
+                .append("\n");
+        if(mQuantity == 0) {
+            mPriceMessage.append("Free");
+        } else {
+            mPriceMessage.append("Quantity: ")
+                    .append(mQuantity)
+                    .append("\n")
+                    .append("Total: ")
+                    .append("NT$")
+                    .append(mPrice * mQuantity)
+                    .append("\n")
+                    .append("Thank you!")
+                    .append("\n");
         }
     }
 
-    private void resetTotolPrice() {
-        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
-        priceTextView.setText("");
+    public void clickToppings(View view) {
+        resetPriceMessageString();
+        displayPriceMessage();
     }
 
+    private void resetPriceMessageString() {
+        clearPriceMessageString();
+        mPriceMessage.append("臭豆腐")
+                .append("NT$").append(mPrice);
+    }
 }
